821b8bf8de9af3ed0a6bf4c873f805804e7cb597
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-19 21:24:47 -0600

    commit1

--- IntegerLoop/Program.cs
+++ IntegerLoop/Program.cs
@@ -7,7 +7,11 @@ namespace IntegerLoop
         public static void Main()
         {
             // TODO: Print even integers from 0 to 50.
-
+            for (int num = 1; num < 51; num++)
+            {
+                if (num % 2 == 0)
+                    Console.WriteLine(num);
+            }
             Console.ReadLine();
         }
     }
